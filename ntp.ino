b89fa6580a9870c3f7e5a8e06a4ebe1b835bef63
wedexyz <38834987+wedexyz@users.noreply.github.com>
2020-03-23 01:03:03 +0700

    Update ntp.ino

--- ntp.ino
+++ ntp.ino
@@ -1,17 +1,43 @@
 #include <WiFi.h>
 #include "time.h"
 
-const char* ssid       = "S50";
-const char* password   = "widhiwinata";
+const char* ssid       = "SSID";
+const char* password   = "PAss";
 
 const char* ntpServer = "asia.pool.ntp.org";
 
 const long  gmtOffset_sec = 21600;
 const int   daylightOffset_sec = 3600;
 
+int second;
+int minute;
+int hour;
+int day;
+int month;
+int year;
+int weekday;
+long current;
+struct tm timeinfo;
+
+  /*
+struct tm
+{
+int    tm_sec;   //   Seconds [0,60]. 
+int    tm_min;   //   Minutes [0,59]. 
+int    tm_hour;  //   Hour [0,23]. 
+int    tm_mday;  //   Day of month [1,31]. 
+int    tm_mon;   //   Month of year [0,11]. 
+int    tm_year;  //   Years since 1900. 
+int    tm_wday;  //   Day of week [0,6] (Sunday =0). 
+int    tm_yday;  //   Day of year [0,365]. 
+int    tm_isdst; //   Daylight Savings flag. 
+}
+ */  
+
+
 void printLocalTime()
 {
-  struct tm timeinfo;
+
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
@@ -41,10 +67,25 @@ void loop()
 {
   delay(1000);
   printLocalTime();
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  second = timeinfo.tm_sec;
+  minute = timeinfo.tm_min;
+  hour = timeinfo.tm_hour;
+  day = timeinfo.tm_mday;
+  month = timeinfo.tm_mon + 1;
+  year = timeinfo.tm_year + 1900;
+  weekday = timeinfo.tm_wday +1;
+  Serial.print("Time from variables:  ");
+  Serial.print(day);
+  Serial.print(".");
+  Serial.print(month);
+  Serial.print(".");
+  Serial.print(year);
+  Serial.print(" --- ");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(second);
+  
 }
